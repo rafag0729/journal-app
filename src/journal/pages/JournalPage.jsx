135d669dd09ccb83c8f9275947f848a78f8f408c
Rafael Gallego <rafaga_729@hotmail.com>
2022-08-16 09:58:25 -0500

    Material UI and login config added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,12 @@
+import { MailOutline } from "@mui/icons-material"
+import { Typography } from "@mui/material"
 
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+      {/* <MailOutline /> */}
+    </>
   )
 }
