45f568033499969a377cb71e2dc1864d8c5507e9
Rafael Gallego <rafaga_729@hotmail.com>
2022-08-16 16:31:57 -0500

    Notes Structure added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,16 @@
-import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      {/* <MailOutline /> */}
-    </>
+    <JournalLayout>
+      {/* <Typography>Mollit laboris elit eu pariatur in tempor ipsum. Incididunt ipsum culpa non aute id ea pariatur pariatur. Voluptate eu in ipsum tempor nulla ullamco ea laborum ipsum id aliqua sint cupidatat ut. Nostrud eu aliqua ea cillum dolore eu nisi. Excepteur pariatur cillum anim deserunt reprehenderit ullamco proident labore ut velit. Labore qui fugiat tempor aute culpa ex occaecat est esse cillum sint excepteur eu.</Typography> */}
+
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+    </JournalLayout>
   )
 }
